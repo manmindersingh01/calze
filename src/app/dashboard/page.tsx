b1c168426c5b7fa940116b3197c40154bac80cce
manminder singh <bewithme2407@gmail.com>
2024-11-18 14:40:27 +0530

    bug

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,8 +12,6 @@ import {
   PlusCircle,
   Settings,
   Trash,
-  User2,
-  UserX2,
 } from "lucide-react";
 import { FaUsers } from "react-icons/fa";
 import { Switch } from "@/components/ui/switch";
@@ -27,12 +25,12 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import CopyLinkMenu from "@/components/CopyLinkMenu";
-async function getData(id: string) {
+
+async function fetchData(id: string) {
   const data = await prisma.user.findUnique({
     where: {
       id: id,
     },
-
     select: {
       eventType: {
         select: {
@@ -56,12 +54,14 @@ async function getData(id: string) {
 
   return data;
 }
+
 export default async function Dashboard() {
   const session = await requireUser();
-  const data = await getData(session.user?.id as string);
+  const data = await fetchData(session.user?.id as string);
+
   return (
     <>
-      {data.eventType.length == 0 ? (
+      {data.eventType.length === 0 ? (
         <EmptyState
           title="You have no event types"
           description="You can create event types by clicking the button below"
@@ -76,7 +76,7 @@ export default async function Dashboard() {
                 Event Types
               </h1>
               <p className="text-muted-foreground text-sm">
-                create and manage your events right here
+                Create and manage your events right here
               </p>
             </div>
             <Button asChild variant={"outline"}>
@@ -87,69 +87,66 @@ export default async function Dashboard() {
             </Button>
           </div>
           <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
-            {data.eventType.map((val) => {
-              return (
-                <div
-                  key={val.id}
-                  className="overflow-hidden border shadow rounded-lg relative"
-                >
-                  <div className=" absolute top-2 right-2">
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button>
-                          <Settings className="size-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Event</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuGroup>
-                          <DropdownMenuItem asChild>
-                            <Link href={`/${data.username}/${val.url}`}>
-                              <ExternalLink />
-                              Preview
-                            </Link>
-                          </DropdownMenuItem>
-                          <DropdownMenuItem>
-                            <CopyLinkMenu
-                              meetingUrl={`${process.env.NEXT_PUBLIC_URL}/${data.username}/${val.url}`}
-                            />
-                          </DropdownMenuItem>
-                          <DropdownMenuItem asChild>
-                            <Link href={`/dashboard/event/${val.id}`}>
-                              {" "}
-                              <Pen className="mr-2 size-4" />
-                              Edit
-                            </Link>
-                          </DropdownMenuItem>
-                        </DropdownMenuGroup>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                          <Trash className="mr-2 size-4" /> Delete
+            {data.eventType.map((val) => (
+              <div
+                key={val.id}
+                className="overflow-hidden border shadow rounded-lg relative"
+              >
+                <div className="absolute top-2 right-2">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button>
+                        <Settings className="size-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>Event</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuGroup>
+                        <DropdownMenuItem asChild>
+                          <Link href={`/${data.username}/${val.url}`}>
+                            <ExternalLink />
+                            Preview
+                          </Link>
                         </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </div>
-                  <Link href={"/"} className="flex items-center p-5">
-                    <div className="flex-shrink-0">
-                      <FaUsers className="size-6" />
-                    </div>
-                    <div className="ml-5 w-0 flex-1">
-                      <dl>
-                        <dt className="text-sm font-medium text-muted-foreground">
-                          {val.duration} minutes meeting
-                        </dt>
-                        <dd className="text-lg font-medium">{val.title}</dd>
-                      </dl>
-                    </div>
-                  </Link>
-                  <div className="bg-muted px-5 py-3 flex justify-between items-center">
-                    <Switch className="border border-black" />
-                    <Button>Edit event</Button>
-                  </div>
+                        <DropdownMenuItem>
+                          <CopyLinkMenu
+                            meetingUrl={`${process.env.NEXT_PUBLIC_URL}/${data.username}/${val.url}`}
+                          />
+                        </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                          <Link href={`/dashboard/event/${val.id}`}>
+                            <Pen className="mr-2 size-4" />
+                            Edit
+                          </Link>
+                        </DropdownMenuItem>
+                      </DropdownMenuGroup>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>
+                        <Trash className="mr-2 size-4" /> Delete
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
-              );
-            })}
+                <Link href={"/"} className="flex items-center p-5">
+                  <div className="flex-shrink-0">
+                    <FaUsers className="size-6" />
+                  </div>
+                  <div className="ml-5 w-0 flex-1">
+                    <dl>
+                      <dt className="text-sm font-medium text-muted-foreground">
+                        {val.duration} minutes meeting
+                      </dt>
+                      <dd className="text-lg font-medium">{val.title}</dd>
+                    </dl>
+                  </div>
+                </Link>
+                <div className="bg-muted px-5 py-3 flex justify-between items-center">
+                  <Switch className="border border-black" />
+                  <Button>Edit event</Button>
+                </div>
+              </div>
+            ))}
           </div>
         </>
       )}
